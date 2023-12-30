46c034727877385c14e8da6ac669c304d1b6d8b6
nancymatijas <123400540+nancymatijas@users.noreply.github.com>
2023-12-30 17:48:15 +0100

    Promjene 30.12.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import torch
 from train import train_model, eval_model
 from test import test_model
 from data_loader import get_data_loaders, show_sample_images
@@ -15,13 +16,13 @@ def main():
     train_loader, val_loader, test_loader = get_data_loaders(train_csv, test_csv)
     
     model = EmotionCNN()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    criterion = nn.CrossEntropyLoss()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
     scheduler = ReduceLROnPlateau(optimizer, mode='min', patience=3, factor=0.1, verbose=True)
 
-    train_model(model, optimizer, scheduler, train_loader, val_loader, criterion, num_epochs=15, save_model_every=3, model_save_path=r'C:\Users\nancy\OneDrive\Radna površina\projekt\our_model.pt')
+    criterion = nn.CrossEntropyLoss()
 
-    #train_model(model, optimizer, train_loader, val_loader, criterion, num_epochs=15, save_model_every=3, model_save_path=r'C:\Users\nancy\OneDrive\Radna površina\projekt\our_model.pt')
+    train_model(model, optimizer, scheduler, train_loader, val_loader, criterion, num_epochs=15, save_model_every=5, model_save_path=r'C:\Users\nancy\OneDrive\Radna površina\projekt\RUSU_ProjektPy\our_model.pt')
     test_model(model, test_loader, emotions)
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -8,30 +8,45 @@ class EmotionCNN(nn.Module):
     def __init__(self, num_classes=7):
         super(EmotionCNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.dropout1_conv = nn.Dropout2d(0.5)
         self.bn1 = nn.BatchNorm2d(64)
         self.dropout1 = nn.Dropout2d(0.5)
+
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.dropout2_conv = nn.Dropout2d(0.5)
         self.bn2 = nn.BatchNorm2d(128)
         self.dropout2 = nn.Dropout2d(0.5)
+
         self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.dropout3_conv = nn.Dropout2d(0.5)
         self.bn3 = nn.BatchNorm2d(256)
         self.dropout3 = nn.Dropout2d(0.5)
+
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         self.fc1 = nn.Linear(256 * 8 * 8, 512)
         self.fc2 = nn.Linear(512, num_classes)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.dropout1(self.bn1(x))
+        x = self.dropout1_conv(x)
+        x = self.bn1(x)
+        x = self.dropout1(x)
+        
         x = self.pool(F.relu(self.conv2(x)))
-        x = self.dropout2(self.bn2(x))
+        x = self.dropout2_conv(x)
+        x = self.bn2(x)
+        x = self.dropout2(x)
+        
         x = self.pool(F.relu(self.conv3(x)))
-        x = self.dropout3(self.bn3(x))
+        x = self.dropout3_conv(x)
+        x = self.bn3(x)
+        x = self.dropout3(x)
+        
         x = x.view(-1, 256 * 8 * 8)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
-
+    
 # Augmentation and transformation
 data_transform = transforms.Compose([
     RandomResizedCrop(64),
